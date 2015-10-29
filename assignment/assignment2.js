9601feda5fcabec21fe87a5233aff247eada3c7a
Rory Dungan <rorydungan@gmail.com>
2015-10-29 22:21:53 +1000

    Added code for loading meshes

--- assignment/assignment2.js
+++ assignment/assignment2.js
@@ -19,7 +19,7 @@ function initWebGL(canvas) {
     return gl;
 }
 
-function init() {
+function init(birdMesh) {
     var canvas = document.getElementById("gl-canvas");
 
     gl = initWebGL(canvas);
@@ -44,6 +44,80 @@ function render() {
     requestAnimFrame(render);
 }
 
+function loadMeshData(string) {
+
+    var lines = string.split("\n");
+    var positions = [];
+    var normals = [];
+    var vertices = [];
+    for ( var i = 0 ; i < lines.length ; i++ ) {
+        
+        var parts = lines[i].trimRight().split(' ');
+        if ( parts.length > 0 ) {
+
+            switch(parts[0]) {
+                case 'v':  
+                    positions.push(
+                        vec3.fromValues(
+                            parseFloat(parts[1]),
+                            parseFloat(parts[2]),
+                            parseFloat(parts[3])
+                        ));
+                    break;
+                case 'vn':
+                    normals.push(
+                        vec3.fromValues(
+                            parseFloat(parts[1]),
+                            parseFloat(parts[2]),
+                            parseFloat(parts[3])
+                        ));
+                    break;
+                case 'f': 
+                {
+                    var f1 = parts[1].split('//');
+                    var f2 = parts[2].split('//');
+                    var f3 = parts[3].split('//');
+                    Array.prototype.push.apply(
+                        vertices, positions[parseInt(f1[0]) - 1]);
+                    // Array.prototype.push.apply(
+                        // vertices, normals[parseInt(f1[2]) - 1]);
+                    Array.prototype.push.apply(
+                        vertices, positions[parseInt(f2[0]) - 1]);
+                    // Array.prototype.push.apply(
+                        // vertices, normals[parseInt(f2[2]) - 1]);
+                    Array.prototype.push.apply(
+                        vertices, positions[parseInt(f3[0]) - 1]);
+                    // Array.prototype.push.apply(
+                        // vertices, normals[parseInt(f3[2]) - 1]);
+                    break;
+                }
+            }
+        }
+    }
+    return {
+        // primitiveType: 'TRIANGLES',
+        primitiveType: 'GL_LINES',
+        vertices: new Float32Array(vertices),
+        vertexCount: vertices.length / 3,
+        material: {ambient: 0.2, diffuse: 0.5, shininess: 10.0}
+    };
+}
+
+function loadMesh(filename, callback) {
+    $.ajax({
+        url: filename,
+        dataType: 'text'
+    }).done(function(data) {
+        console.log("Loaded mesh \"" + filename+ "\"");
+        var meshData = loadMeshData(data);
+        callback(meshData);
+    }).fail(function() {
+        alert("Failed to retrieve [" + filename + "]");
+    });
+}
+
 $(document).ready(function() {
-    init();
+    loadMesh("Bird.obj", function(birdMesh) {
+        init(birdMesh);
+    });
 })
